308f74770a9fe72a5c2a50184e9eb18858c5e615
sakakaito <k.bibifuu@gmail.com>
2024-03-05 22:09:42 +0900

    aaaa

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+            ['id' => '1',
+            'over_name' => 'うずまき',
+            'under_name' => 'ナルト',
+            'over_name_kana'=> 'ウズマキ',
+            'under_name_kana'=>'ナルト',
+            'mail_address'=>'naruto@gmail.com',
+            'sex'=> '1',
+            'birth_day'=>'2000-10-10',
+            'password'=>'naruto0000']
+        ]);
     }
 }
